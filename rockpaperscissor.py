2565db54cc5c2ed9f8e11f75e2136cfbad3756f4
Arnav Motwani <50354529+ArnavMotwani@users.noreply.github.com>
2019-10-21 17:16:56 +0530

    Corrected spellings and cleaned up the code a bit.

--- rockpaperscissor.py
+++ rockpaperscissor.py
@@ -1,7 +1,6 @@
 import random as r
 
-
-items = ["Rock", "Paper", "siccosor","exit"]
+items = ["Rock", "Paper", "Scissor", "exit"]
 computer = items[r.randint(0, 2)]
 computerWon = 0
 
@@ -11,12 +10,12 @@ player = False
 tie = 0
 temp = 0
 
-while player == False:
+while not player:
     player = items[int(input("enter either 0 or 1 or 2 or 3 to exit \t"))]
     if player == "Rock":
         if computer == "Paper":
             computerWon += 1
-            print("Sorry Buddy,The Computer Won ",player, "by", computer)
+            print("Sorry Buddy,The Computer Won ", player, "by", computer)
         elif computer == "Rock":
             print("tie")
             tie += 1
@@ -24,20 +23,20 @@ while player == False:
             playerWon += 1
             print("Congratulations, Player won ", computer, "by", player)
     elif player == "Paper":
-        if computer == "Siccosor":
+        if computer == "Scissor":
             computerWon += 1
-            print("Your Bad Luck, Computer Won ",player, "by", computer)
+            print("Your Bad Luck, Computer Won ", player, "by", computer)
         elif computer == "Paper":
             print("tie")
             tie += 1
         else:
             playerWon += 1
             print("Im the king of the world, Player won ", computer, "by", player)
-    elif player == "siccosor":
+    elif player == "Scissor":
         if computer == "Rock":
             computerWon += 1
             print("Im gonna take over the world, Computer Won ", player, "by", computer)
-        elif computer == "Siccosor":
+        elif computer == "Scissor":
             print("tie")
             tie += 1
         else:
@@ -46,12 +45,10 @@ while player == False:
     elif player == "exit":
         break
     else:
-            print("Invalid number only 0 or 1 or 2 else GET LOST")
+        print("Invalid number only 0 or 1 or 2 else GET LOST")
     player = False
     computer = items[r.randint(0, 2)]
 
 print("player Scored ", playerWon)
-print("computer scored", computerWon )
+print("computer scored", computerWon)
 print("ties", tie)
-
-
